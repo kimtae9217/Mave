fb0df80de1ef7fe68db4f943dcc7473dd8243597
taewon <kimtae9217@gmail.com>
2021-05-13 17:30:52 +0900

    feat(Mave): 다이어리 UI 수정 및 page2_sub와 page2_sub_answer 클릭 이벤트 코드 추가

--- app/src/main/java/com/example/mave/Page2_sub.java
+++ app/src/main/java/com/example/mave/Page2_sub.java
@@ -1,14 +1,31 @@
 package com.example.mave;
 
+import android.content.Intent;
 import android.os.Bundle; import androidx.annotation.Nullable; import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle; import androidx.annotation.Nullable; import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class Page2_sub extends AppCompatActivity
-{
+import androidx.annotation.Nullable; import androidx.appcompat.app.AppCompatActivity;
+
+public class Page2_sub extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.fragment_page2_sub);
+
+        TextView textview = (TextView)findViewById(R.id.todayQuestion); // 오늘의 질문 버튼
+
+        textview.setOnClickListener(new View.OnClickListener() { // 오늘의 질문을 눌렀을 때 이벤트
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Page2_sub.this, Page2_sub_answer.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
