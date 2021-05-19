e0cbd00bf677ea6eb6f3f3137b36da771e4a16d0
rhqudco <rhqudco1204@naver.com>
2021-05-20 03:54:43 +0900

    Answer add

---
+++ app/src/main/java/com/example/mave/Answer_Item.java
@@ -0,0 +1,14 @@
+package com.example.mave;
+
+public class Answer_Item {
+
+    private String Answer;
+
+    public void setAnswer(String answer) {
+        this.Answer = answer;
+    }
+
+    public String getAnswer() {
+        return Answer;
+    }
+}
---
+++ app/src/main/java/com/example/mave/ListViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.mave;
+
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+    public class ListViewAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private ArrayList<Answer_Item> listItems = new ArrayList<Answer_Item>();
+
+        public ListViewAdapter(Context context){
+            this.mContext = context;
+        }
+        @Override
+        public int getCount() {
+            return listItems.size();
+        }
+        @Override
+        public Object getItem(int i) {
+            return listItems.get(i);
+        }
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            // item.xml 레이아웃을 inflate해서 참조획득
+            if(convertView == null){
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.answer_add_list, parent, false);
+            }
+
+            // item.xml 의 참조 획득
+            TextView txt_title = (TextView)convertView.findViewById(R.id.txt_title);
+            Answer_Item listItem = listItems.get(position);
+            // 가져온 데이터를 텍스트뷰에 입력
+            txt_title.setText(listItem.getAnswer());
+            return convertView;
+        }
+
+        public void addItem(String answer){
+            Answer_Item listItem = new Answer_Item();
+            listItem.setAnswer(answer);
+            listItems.add(listItem);
+        }
+    }
+
