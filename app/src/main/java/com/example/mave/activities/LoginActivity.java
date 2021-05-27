9db802008da4d587d9a624d227f646a8b096e45d
taewon <kimtae9217@gmail.com>
2021-05-27 18:51:46 +0900

    LoginActivity 버튼 애니메이션

--- app/src/main/java/com/example/mave/activities/LoginActivity.java
+++ app/src/main/java/com/example/mave/activities/LoginActivity.java
@@ -4,25 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.mave.Diary.Page2_sub;
-import com.example.mave.Diary.Page2_sub_answer;
 import com.example.mave.Dto.LoginRequest;
 import com.example.mave.Dto.LoginResponse;
 import com.example.mave.R;
-import com.example.mave.models.ApiResponse;
-import com.example.mave.retrofitUtil.ApiClient;
-import com.example.mave.retrofitUtil.ApiInterface;
 import com.example.mave.service.MemberRetrofitService;
+import com.royrodriguez.transitionbutton.TransitionButton;
+import com.royrodriguez.transitionbutton.utils.WindowUtils;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,49 +27,82 @@ import static com.example.mave.activities.RegisterActivity.TAG;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TransitionButton transitionLoginBtn;
+    boolean isSuccessful;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        WindowUtils.makeStatusbarTransparent(this);
+        getSupportActionBar().hide();
+
         EditText userID = findViewById(R.id.userID);
         EditText userPW = findViewById(R.id.userPW);
-        Button loginBtn = findViewById(R.id.loginBtn);
+        transitionLoginBtn = findViewById(R.id.transition_button);
         TextView SignUp = findViewById(R.id.SignUp);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        transitionLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("http://192.168.211.1:8080/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
 
-                MemberRetrofitService memberRetrofitService = retrofit.create(MemberRetrofitService.class);
-                LoginRequest request = new LoginRequest(userID.getText().toString(), userPW.getText().toString());
-                Call<LoginResponse> call = memberRetrofitService.login(request);
+                transitionLoginBtn.startAnimation(); // 로그인 버튼 탭 눌렀을 때
 
-                call.enqueue(new Callback<LoginResponse>() {
+                // Do your networking task or background work here.
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
                     @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                        if (response.isSuccessful()) {
-                            LoginResponse body = response.body();
-                            Log.d(TAG, "response 성공!!");
-//                            textTest.setText(body.getUserId().toString());
+                    public void run() {
+                        /* ---------------------------Retrofit 통신--------------------------------------- */
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl("http://192.168.211.1:8080/")
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .build();
+
+                        MemberRetrofitService memberRetrofitService = retrofit.create(MemberRetrofitService.class);
+                        LoginRequest request = new LoginRequest(userID.getText().toString(), userPW.getText().toString());
+                        Call<LoginResponse> call = memberRetrofitService.login(request);
+
+                        call.enqueue(new Callback<LoginResponse>() {
+                            @Override
+                            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                                if (response.isSuccessful()) {
+                                    LoginResponse body = response.body();
+                                    Log.d(TAG, "response 성공!!");
+                                    isSuccessful = true;
+                                } else {
+                                    Log.d(TAG, "response 실패 ㅠㅠ");
+                                    isSuccessful = false;
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                                Log.d(TAG, "onFailure => " + t.getMessage());
+                                isSuccessful = false;
+                            }
+                        });
+
+                        // Choose a stop animation if your call was succesful or not
+                        if (isSuccessful) {
+                            transitionLoginBtn.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
+                                @Override
+                                public void onAnimationStopEnd() {
+                                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                    startActivity(intent);
+                                }
+                            });
+
                         } else {
-                            Log.d(TAG, "response 실패 ㅠㅠ");
+                            transitionLoginBtn.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
                         }
                     }
-
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        Log.d(TAG, "onFailure => " + t.getMessage());
-                    }
-                });
-
+                }, 1000);
             }
         });
+
 
         SignUp.setOnClickListener(new View.OnClickListener() {@Override
         public void onClick(View v) {
